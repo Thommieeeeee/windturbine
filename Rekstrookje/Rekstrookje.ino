07683e09e121b5e68e077f290c306402a6ec1ed5
stikkie13 <stikkie13@gmail.com>
2025-06-03 13:44:20 +0200

    rekstrookje nu in procenten

--- Rekstrookje/Rekstrookje.ino
+++ Rekstrookje/Rekstrookje.ino
@@ -1,5 +1,5 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
+// #include <WiFi.h>
+// #include <PubSubClient.h>
 #include "HX711.h"
 
 // === WIFI ===
@@ -13,85 +13,93 @@ const int mqtt_port = 1883;
 const char* access_token = "L6MgZuzAtW6W7iDfgwdf";
 
 // HX711 pins
-#define DOUT 35
-#define CLK 34
+#define DOUT 26
+#define CLK 25
 
 HX711 scale;
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+int zeroPoint = 0;
+float maxChange = 1;
+
+// WiFiClient espClient;
+// PubSubClient client(espClient);
 
 unsigned long lastMsg = 0;
-const long interval = 5000;
+const long interval = 1;
 
-void setup_wifi() {
-  delay(10);
-  Serial.println();
-  Serial.print("Connecting to WiFi");
+// void setup_wifi() {
+//   delay(10);
+//   Serial.println();
+//   Serial.print("Connecting to WiFi");
 
-  WiFi.begin(ssid, WPA2_AUTH_PEAP, username, username, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+//   WiFi.begin(ssid, WPA2_AUTH_PEAP, username, username, password);
+//   while (WiFi.status() != WL_CONNECTED) {
+//     delay(500);
+//     Serial.print(".");
+//   }
 
-  Serial.print("\nWiFi connected, status: ");
-  Serial.println(WiFi.status());
-}
+//   Serial.print("\nWiFi connected, status: ");
+//   Serial.println(WiFi.status());
+// }
 
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Verbinding maken met MQTT server...");
-    if (client.connect("ESP32", access_token, NULL)) {
-      Serial.println("MQTT verbonden!");
-    } else {
-      Serial.print("Foutcode: ");
-      Serial.print(client.state());
-      Serial.println(" — opnieuw proberen in 5s");
-      delay(5000);
-    }
-  }
-}
+// void reconnect() {
+//   while (!client.connected()) {
+//     Serial.print("Verbinding maken met MQTT server...");
+//     if (client.connect("ESP32", access_token, NULL)) {
+//       Serial.println("MQTT verbonden!");
+//     } else {
+//       Serial.print("Foutcode: ");
+//       Serial.print(client.state());
+//       Serial.println(" — opnieuw proberen in 5s");
+//       delay(5000);
+//     }
+//   }
+// }
 
 void setup() {
   Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, mqtt_port);
+  // setup_wifi();
+  // client.setServer(mqtt_server, mqtt_port);
 
   // HX711 setup
   scale.begin(DOUT, CLK);
-  scale.set_scale(1234.5);  // Kalibratie
-  scale.tare();
+  scale.set_scale(0);  // Kalibratie
+  scale.tare(10);
   Serial.println("HX711 klaar!");
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+  // if (!client.connected()) {
+  //   reconnect();
+  // }
+  // client.loop();
 
   unsigned long now = millis();
   if (now - lastMsg > interval) {
     lastMsg = now;
 
-    long raw = scale.read();
+    long raw = scale.read_median(10);
     Serial.print("Raw waarde: ");
-    Serial.println(raw);
+    Serial.println(float(zeroPoint - raw)/maxChange);
 
-    float gewicht = scale.get_units();
-    Serial.print("Gewicht: ");
-    Serial.println(gewicht);
+    // // JSON payload opbouwen
+    // String payload = "{\"g\":";
+    // payload += String(gewicht, 2);
+    // payload += "}";
 
-    // JSON payload opbouwen
-    String payload = "{\"g\":";
-    payload += String(gewicht, 2);
-    payload += "}";
+    // Serial.print("MQTT payload: ");
+    // Serial.println(payload);
 
-    Serial.print("MQTT payload: ");
-    Serial.println(payload);
-
-    // Stuur naar ThingsBoard
-    client.publish("v1/devices/me/telemetry", (char*)payload.c_str());
+    // // Stuur naar ThingsBoard
+    // client.publish("v1/devices/me/telemetry", (char*)payload.c_str());
+  }
+  if(Serial.available()){
+    int in = Serial.read();
+    if (in == '1'){
+      zeroPoint = scale.read_average(10);
+    }
+    if (in == '2'){
+      maxChange = scale.read_average(10);
+    }
   }
 }
